41a412dcce941274a4d65e2d3ec4f14e7c9f7dcc
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-12-07 14:15:28 -0300

    feat:  adicionar modelo PerfilCliente com campos básicos

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PerfilCliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cnh = models.CharField(max_length=20, unique=True)
+    telefone = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Perfil de {self.user.username}"
+    
+    # função para retornar histórico
+    def get_historico_alugueis(self):
+        return
+    
+    # função para retornar alugueis ativos
+    def get_alugueis_ativos(self):
+        return
+    
