f77255142d98445fa1155f54c35491a9337e64f0
Theuszzz <matheus.fernandes.r10@gmail.com>
2025-12-09 00:55:04 -0300

    feat: serializer para a ação alugado/disponivel

--- cars/serializers.py
+++ cars/serializers.py
@@ -6,3 +6,17 @@ class CarroSerializer(serializers.ModelSerializer):
         model = Carro
         fields = ['modelo', 'placa', 'ano', 'valor_diaria', 'status']
         read_only_fields = ['status']  # impede alterar direto
+
+class AlterarStatus(serializers.Serializer):
+    status = serializers.ChoiceField(choices=Carro.STATUS_CHOICES)
+
+    def update(self, instance, validated_data):
+        status = validated_data.get('status')
+        if status == 'alugado':
+            instance.marcar_como_alugado()
+        elif status == 'disponivel':
+            instance.marcar_como_disponivel()
+        return instance
+
+    def create(self, validated_data):
+        pass  # Não é necessário para este serializer    
