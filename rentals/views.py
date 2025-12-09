386af76854ae9c10aadca04802472a8b8793051e
Maria Luiza <marialluantunes@gmail.com>
2025-12-09 16:10:00 -0300

    feat(rentals): implementação do AluguelViewSet com controle de acesso

--- rentals/views.py
+++ rentals/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Aluguel
+from .serializers import AluguelSerializer
+from users.permissions import IsFuncionario  
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
+
+class AluguelViewSet(viewsets.ModelViewSet):
+    queryset = Aluguel.objects.all()
+    serializer_class = AluguelSerializer
+    permission_classes = [IsAuthenticated,  IsFuncionario | IsAdminUser]
+
+
