39a87c8b3d42a8cd8b2d0520038c56059760254e
Maria Luiza <marialluantunes@gmail.com>
2025-12-09 16:17:40 -0300

    feat(cars): criar viewset para CRUD de Carros com permissões de funcionários ou admin

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Carro
+from .serializers import CarroSerializer
+from users.permissions import IsFuncionario  
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
+
+class CarroViewSet(viewsets.ModelViewSet):
+    queryset = Carro.objects.all()
+    serializer_class = CarroSerializer
+    permission_classes = [IsAuthenticated,  IsFuncionario | IsAdminUser]
+
