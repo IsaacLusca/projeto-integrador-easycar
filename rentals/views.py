1869a15edac5415242b69d2125ef0930e265bf02
Maria Luiza <marialluantunes@gmail.com>
2025-12-16 01:22:50 -0300

    Implementa django-filter para consulta de alugueis e adiciona campo id no CarroSerializer

--- rentals/views.py
+++ rentals/views.py
@@ -3,12 +3,15 @@ from .models import Aluguel
 from .serializers import AluguelSerializer
 from rest_framework import viewsets, serializers
 from users.permissions import IsFuncionarioOuSuperuser
+from .filters import AluguelFilter 
 
 
 class AlugarViewSet(viewsets.ModelViewSet):
     queryset = Aluguel.objects.all()
     serializer_class = AluguelSerializer
     permission_classes = [IsFuncionarioOuSuperuser]
+    filterset_class = AluguelFilter #define quais campos podem ser filtrados
+
 
  
     
